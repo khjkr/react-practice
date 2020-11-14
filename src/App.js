3176a33090a39ba333aa1427b994fabfc0e7e56e
Hyunjun Kim <hyunjun5216@naver.com>
2020-11-14 17:55:03 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
+import HeaderContainer from './components/HeaderContainer';
 
 // 사용자 정의 컴포넌트
 function Greeting(props) {
@@ -15,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HeaderContainer/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <Greeting name={ "Pengsu" }/>
